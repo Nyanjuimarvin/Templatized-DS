47f1579c1ff739f51735bfe0c8500e48a08f48c6
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-14 23:42:29 +0300

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nyanjuimarvin/C++ Projects/Templatized DS
+# Build directory: /home/nyanjuimarvin/C++ Projects/Templatized DS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
