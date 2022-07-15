154fb3fe838636da3c974c08973af0fedd3f7063
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-15 13:56:15 +0300

    Add class ListNode private members and class List friend declaration

--- includes/ListNodes.h
+++ includes/ListNodes.h
@@ -4,18 +4,29 @@
  * Created:
  *   July 15, 2022, 1:42:24 PM GMT+3
  * Last edited:
- *   July 15, 2022, 1:44:47 PM GMT+3
+ *   July 15, 2022, 1:56:03 PM GMT+3
  * Auto updated?
  *   Yes
  *
  * Description:
  *   Template class ListtNode definition
-**/
+ **/
 
 #ifndef LISTNODE_H
 #define LISTNODE_H
 
+// Forward Declaration of class List
+template <typename NODETYPE> class List;
 
+template <typename NODETYPE>
+class ListNode
+{
+    // Make class List friend -> Access of ListNode private members
+    friend class List<NODETYPE>;
+
+private:
+    NODETYPE data;
+    ListNode<NODETYPE> *nextPtr;
+}
 
 #endif
-
