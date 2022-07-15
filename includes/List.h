27b907ba57bdf3aba361a7ea71b085171f1696ad
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-15 14:34:59 +0300

    Add class List private members

--- includes/List.h
+++ includes/List.h
@@ -4,14 +4,24 @@
  * @brief Template class List definition
  * @version 0.1
  * @date 2022-07-15
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream>
+#include "ListNodes.h"
 
+template <typename NODETYPE>
+class List
+{
+
+private:
+    ListNode<NODETYPE> *firstPtr;//ptr to first Node
+    ListNode<NODETYPE> *lastPtr;//ptr to last Node
+}
 
 #endif
