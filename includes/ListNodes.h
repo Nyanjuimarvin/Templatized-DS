9af89917b89347c0862109eb93db9c01c0a70b01
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-15 13:45:12 +0300

    Add ListNodes header file

---
+++ includes/ListNodes.h
@@ -0,0 +1,21 @@
+/* LIST NODES.h
+ *   by Nyanjuimarvin
+ *
+ * Created:
+ *   July 15, 2022, 1:42:24 PM GMT+3
+ * Last edited:
+ *   July 15, 2022, 1:44:47 PM GMT+3
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Template class ListtNode definition
+**/
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+
+
+#endif
+
