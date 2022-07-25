9da4eb65a14243c6fed5b6f3981432bb96b98fa1
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-25 13:06:50 +0300

    add whatToDo function

--- src/runList.cpp
+++ src/runList.cpp
@@ -1,8 +1,29 @@
+/**
+ * @file runList.cpp
+ * @author Nyanjuimarvin
+ * @brief Executable to test the List
+ * @version 0.1
+ * @date 2022-07-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <iostream>
 #include <string>
 
 #include "List.h"
 
-int main(int, char**){
+void whatToDo()
+{
+    std::cout << "Enter one of the following:\n"
+              << " 1 to insert at beginning of list\n"
+              << " 2 to insert at end of list\n"
+              << " 3 to delete from beginning of list\n"
+              << " 4 to delete from end of list\n"
+              << " 5 to end list processing\n";
+}
 
+int main(int, char **)
+{
 }
