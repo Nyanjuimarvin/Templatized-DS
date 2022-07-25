ffc975e5d38555f6d6829f2cd7e9520273fc1e2c
Nyanjuimarvin <marnyanjui@gmail.com>
2022-07-25 12:59:38 +0300

    Unstage build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nyanjuimarvin/C++ Projects/Templatized DS
-# Build directory: /home/nyanjuimarvin/C++ Projects/Templatized DS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
